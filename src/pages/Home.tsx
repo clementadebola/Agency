0f1aa6e099a46ee0f99b4967fecd5487b15de338
clement <clementadebola54@gmail.com>
2025-01-11 02:49:20 +0100

    new

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import About from "./About";
+import Services from "./Services";
 
-const Home: React.FC = () => {
-  return (
-  <h1>Welcome to the Home Page!</h1>
+const Home: React.FC = () => (
+  <HomeContainer>
+    <h2>i am home bby</h2>
 
-  )
-};
+    <Section>
+      <About />
+    </Section>
+
+    <Section>
+      <Services />
+    </Section>
+
+  </HomeContainer>
+);
 
 export default Home;
+
+const HomeContainer = styled.div`
+  font-family: "Arial", sans-serif;
+`;
+
+const Section = styled.section`
+  padding: 4rem 2rem;
+`;
